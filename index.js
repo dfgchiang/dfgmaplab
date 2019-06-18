48aa1b0a822eac718c5650992cc0469f9b014075
Dean Chiang <dean.chiang@wildlife.ca.gov>
2019-06-17 18:27:15 -0700

    deanbox mod

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require('http');
+var url = require('url');
+var os = require('os');
+var hostname = os.hostname();
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
@@ -8,7 +11,12 @@ const server = http.createServer((request, response) => {
 const port = process.env.PORT || 1337;
 server.listen(port);
 
-const locotorp = window.location.protocol;
-const host = window.location.hostname;
+function geturl(req) {
+    return url.format({
+        protocol: req.protocol,
+        host: req.get('host')
+    });
+}//NOT USED
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running at //" + hostname + ':' + port);
+//http://localhost:%d", port);--BAD EXAMPLE FROM MSAZURE
